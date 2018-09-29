f9fb2310ae3cb2edd909d8d543da24c5a09a61a5
chikorita157 <chikorita157@chikorita157.com>
2018-09-28 22:10:24 -0400

    Workaround to hide the menu when the App is not active and show it on the iPad when in landscape so the menu doesn't disappear when it becomes active.

--- Shukofukurou-IOS/View Controllers/MainViewController.m
+++ Shukofukurou-IOS/View Controllers/MainViewController.m
@@ -16,14 +16,30 @@
 @end
 
 @implementation MainViewController
+
+- (void)dealloc {
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+}
+
 - (void)viewDidLoad {
     //self.leftViewWidth = 250.0;
     //self.leftViewBackgroundColor = [UIColor colorWithRed:0.5 green:0.65 blue:0.5 alpha:0.95];
     _vcm = [ViewControllerManager getAppDelegateViewControllerManager];
     _vcm.mvc = self;
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(applicationDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(applicationDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    
     self.leftViewAlwaysVisibleOptions = LGSideMenuAlwaysVisibleOnPadLandscape;
 }
-
+- (void)applicationDidEnterBackground:(NSNotification *)notification {
+    self.leftViewAlwaysVisibleOptions = LGSideMenuAlwaysVisibleOnNone;
+    [super viewWillLayoutSubviews];
+    
+}
+- (void)applicationDidBecomeActive:(NSNotification *)notification {
+    self.leftViewAlwaysVisibleOptions = LGSideMenuAlwaysVisibleOnPadLandscape;
+    [super viewDidLayoutSubviews];
+}
 - (void)loadfromdefaults {
     NSString *selectedrow = [NSUserDefaults.standardUserDefaults valueForKey:@"selectedmainview"];
     [self sidebarItemDidChange:selectedrow];
